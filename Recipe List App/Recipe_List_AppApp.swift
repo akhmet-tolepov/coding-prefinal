bb9556579f6a57acb1a8712981d1741834a3de40
akhmet-tolepov <zhakiya190167@gmail.com>
2024-03-31 15:31:47 +0500

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Akhmet Tolepov on 31.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

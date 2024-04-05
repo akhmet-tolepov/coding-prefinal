8d2d72f97f3d946a306a9bdd7317ffc1212b4835
akhmet-tolepov <zhakiya190167@gmail.com>
2024-04-05 21:06:27 +0500

    add recipeFeatureView modify data flow using object modifier create RecipeModel instance in the RecipeTabView

---
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeFeaturedView.swift
+//  Recipe List App
+//
+//  Created by Akhmet Tolepov on 05.04.2024.
+//
+
+import SwiftUI
+
+struct RecipeFeaturedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeFeaturedView()
+}

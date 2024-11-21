0b01922de56d2cdb32ce97d14c6b547a95a3047b
akhmet-tolepov <zhakiya190167@gmail.com>
2024-11-21 23:56:53 +0500

    - start

---
+++ Recipe List App/Views/RecipeHighlights.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeHighlights.swift
+//  Recipe List App
+//
+//  Created by Akhmet Tolepov on 11.04.2024.
+//
+
+import SwiftUI
+
+struct RecipeHighlights: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeHighlights()
+}
